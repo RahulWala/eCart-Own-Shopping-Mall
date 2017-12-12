388ad5d8dc15f9b3890bb025245ebd7e5e1e24bc
RahulWala <walarahul08@gmail.com>
2017-12-12 20:57:10 +0530

    Just Updated

--- app/controller/users.js
+++ app/controller/users.js
@@ -162,7 +162,7 @@ module.exports.controllerFunction = function(app){
 						res.render('error', {title : "User Not Found"});
 					}
 					else{
-						console.log("UF "+userFound);
+						// console.log("UF "+userFound);
 						res.render('product',{user:userFound});
 					}
 				});
@@ -197,28 +197,28 @@ module.exports.controllerFunction = function(app){
 		});
 	});
 
-	/////////////// Deleteing product  ////////////////
-	appRouter.post('/deletePro/:id',auth.isLoggedIn,function(req,res,next){
-		eProduct.findOne({'_id':req.params.id},function(err,result){
-			if(err){
-				res.render('error',{title : "Something Went Wrong"});
-			}else if(result == null || result == undefined || result == ""){
-				eProduct
-				res.render("error",{title : "Product doesn't exist"});
-			}else{
-				// console.log('id is '+req.params.id);
-				eProduct.remove().where({'_id':req.params.id}).exec((function(err,result){
-					if(err){
-						res.render('error',{title : "Something Went Wrong"});
-					}else if(result == null || result == undefined || result == ""){
-						res.render('error',{title : "Product doesn't exists"});
-					}else{
-						res.render("error",{title : "Product Removed Successfully"});
-					}
-				}));
-			}
-		});
-	});
+	// /////////////// Deleteing product  ////////////////
+	// appRouter.post('/deletePro/:id',auth.isLoggedIn,function(req,res,next){
+	// 	eProduct.findOne({'_id':req.params.id},function(err,result){
+	// 		if(err){
+	// 			res.render('error',{title : "Something Went Wrong"});
+	// 		}else if(result == null || result == undefined || result == ""){
+	// 			eProduct
+	// 			res.render("error",{title : "Product doesn't exist"});
+	// 		}else{
+	// 			// console.log('id is '+req.params.id);
+	// 			eProduct.remove().where({'_id':req.params.id}).exec((function(err,result){
+	// 				if(err){
+	// 					res.render('error',{title : "Something Went Wrong"});
+	// 				}else if(result == null || result == undefined || result == ""){
+	// 					res.render('error',{title : "Product doesn't exists"});
+	// 				}else{
+	// 					res.render("error",{title : "Product Removed Successfully"});
+	// 				}
+	// 			}));
+	// 		}
+	// 	});
+	// });
 
 	////////////// Viewing to cart function and making payment function /////////////////
 	appRouter.post('/viewCart/:id',auth.isLoggedIn,function(req,res,next){
@@ -260,9 +260,57 @@ module.exports.controllerFunction = function(app){
 	// 	});
 	// });
 
+	////////////////Deketing from cart///////////////////
+	appRouter.post('/delete/:id',auth.isLoggedIn,function(req,res){
+
+		var getProduct = function(callback){
+			eProduct.findOne({"_id":req.params.id},{new:true},function(err,products){
+				if(err){
+					var myResponse = responseGenerator.generate(true,err,500,null);
+				}else{
+					callback(null,products);
+				}
+			});
+		}
+
+		var getUser = function(arg,callback){
+			eCart.findOne({"_id":req.session.user._id},function(err,users){
+				if(err){
+					var myResponse = responseGenerator.generate(true,err,500,null);
+				}else{
+					callback(null,arg,users);
+				}
+			});
+		}
+
+		var deleteCart = function(arg,arg1,callback){
+			eCart.findOneAndUpdate({"_id":req.session.user._id},{$pull:{"cart":arg._id}},function(err,delCart){
+				if(err){
+					var myResponse = responseGenerator.generate(true,err,500,null);
+				}else{
+					console.log("Arg is : "+arg);
+					var myResponse = responseGenerator.generate(false,"Product Removed From Cart",200,delCart);
+					callback(null,myResponse);
+				}
+			})
+		}
+
+		async.waterfall([
+			getProduct,
+			getUser,
+			deleteCart
+			],function(err,result){
+				if(err){
+					res.render("error",{title : "Something Went Wrong"});
+				}
+				else{
+					res.render("error",{title : "Product Removed From Cart"});
+				}
+			})
+	});
 
 	////////////// Deleting product from product list and cart also //////////////////////
-	appRouter.post('/delete/:id',auth.isLoggedIn,function(req,res){
+	appRouter.post('/deletePro/:id',auth.isLoggedIn,function(req,res){
 
 		//Async function 
 		var getProduct = function(callback){
@@ -271,7 +319,7 @@ module.exports.controllerFunction = function(app){
 					var myResponse = responseGenerator.generate(true,err,500,null);
 					callback(myResponse);
 				}else{
-					console.log(result);
+					// console.log("Showing this "+result);
 					callback(null,result);
 				}
 			});
@@ -300,28 +348,15 @@ module.exports.controllerFunction = function(app){
 			});
 		}
 
-		var deleteCart = function(arg,arg1,callback){
-			eCart.findOneAndUpdate({"_id":req.session.user._id},{$pull:{"cart":{"_id":req.params.id}}},function(err,resultCart){
-				if(err){
-					var myResponse = responseGenerator.generate(true,err,500,null);
-					callback(myResponse);
-				}else{
-					var myResponse = responseGenerator.generate(false,"Product Deleted Successfully",200,resultCart);
-					callback(null,myResponse);
-				}
-			});
-		}
-
 		async.waterfall([
 			getProduct,
 			getUser,
-			deletingProduct,
-			deleteCart
+			deletingProduct
 			],function(err,result){
 				if(err){
 					res.render("error",{title : "Something Went Wrong"});
 				}else{
-					eCart.update({},{$pull : {"cart" : {"_id" : req.params.id}}},{multi : true},function(){
+					eCart.update({},{$pull : {"cart" : {"_id" : req.params.id}}},{multi:true},function(err,result){
 						if(err){
 							res.render("error", {title : "Something Went Wrong"})
 						}else{
